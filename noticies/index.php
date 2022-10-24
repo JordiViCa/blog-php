f65c4869ba65dab7d3fd49733e3da9c1d387e28f
Jordi Vila <jordi.vilacast@gmail.com>
2022-10-24 15:17:43 +0200

    [FEAT] Create noticies

---
+++ noticies/index.php
@@ -0,0 +1,8 @@
+<?php
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/includes/header.php";
+    $backUrl = $_SERVER['REQUEST_URI'];
+?>
+
+<?php
+    include_once $_SERVER['DOCUMENT_ROOT'] . "./includes/footer.php";
+?>
