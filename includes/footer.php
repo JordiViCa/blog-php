ec909ebc633d1418de4b39bc47365a7cea2cc5c6
Jordi Vila <jordi.vilacast@gmail.com>
2022-10-30 11:57:11 +0100

    [Fix] Remove session destroy

--- includes/footer.php
+++ includes/footer.php
@@ -4,8 +4,6 @@
     <?php
         include_once  $_SERVER['DOCUMENT_ROOT'] . "/includes/forms/login.php";
         include_once  $_SERVER['DOCUMENT_ROOT'] . "/includes/forms/register.php";
-        session_unset();
-        session_destroy();
     ?>
 </body>
 </html>
