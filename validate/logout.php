7d0c63908b6e0695f4c1effa553bb3e8197b125f
Jordi Vila <jordi.vilacast@gmail.com>
2022-10-30 11:59:26 +0100

    [Fix] Remove cookies and session on logout

--- validate/logout.php
+++ validate/logout.php
@@ -1,11 +1,7 @@
 <?php
-    if (isset($_COOKIE["user"])) {
-        unset($_COOKIE["user"]);
-        setcookie('user', null, -1, '/'); 
-    }
-    if (isset($_COOKIE["password"])) {
-        unset($_COOKIE["password"]);
-        setcookie('password', null, -1, '/'); 
-    }
+    session_start();
+    unset($_SESSION["id"]);
+    unset($_COOKIE["remember_me"]);
+    setcookie('remember_me', null, -1, '/'); 
     header("Location: /");
 ?>
