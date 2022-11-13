76a9aa3d0a31cb955cbffc24a26b69e436c86588
Jordi Vila <jordi.vilacast@gmail.com>
2022-11-13 18:58:54 +0100

    [FIX] Display footer and add script

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,10 @@
-    <footer class="py-10 bg-slate-500 text-white text-4xl font-bold text-center">
+    <footer class="py-10 bg-gray-500 text-white text-4xl font-bold text-center">
         FOOTER ENCARA NO DECIDIT
     </footer>
     <?php
         include_once  $_SERVER['DOCUMENT_ROOT'] . "/includes/forms/login.php";
         include_once  $_SERVER['DOCUMENT_ROOT'] . "/includes/forms/register.php";
     ?>
+    <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
 </body>
 </html>
